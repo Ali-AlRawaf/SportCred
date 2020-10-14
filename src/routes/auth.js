0c53e18d8d6734ba9ad6794d97fe3737fcfe0925
Ani <anirudha.kanodia@hotmail.com>
2020-10-14 12:41:24 +0530

    [BIN-2] complete basic signup validations

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user')
+const {registrationValidation} = require('../validations/user_validations');
 
 router.post('/register', async (req, res) => {
+
+  const {error} = registrationValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message)
 
   const user = new User({
     username: req.body.username,
--- src/validations/user_validations.js
+++ src/validations/user_validations.js
@@ -2,35 +2,20 @@ const Joi = require('@hapi/joi')
 
 const registrationValidation = (data) => {
 
-  const schema = {
-    username: Joi.string()
-              .min(5)
-              .max(25)
-              .required(),
+  const schema = Joi.object({
+    username: Joi.string().min(5).max(25).required(),
 
-    email: Joi.string(),
-           .min(5)
-           .max(255)
-           .required()
-           .email(),
+    email: Joi.string().min(5).max(255).required().email(),
 
-    status: Joi.string()
-            .max(200),
+    status: Joi.string().max(200),
 
-    bio: Joi.string()
-         .max(255),
+    bio: Joi.string().max(255),
 
-    acs: Joi.number()
-         .integer(),
-         .required(),
+    password: Joi.string().min(6).required(),
 
-    password: Joi.string()
-              .min(6)
-              .required(),
+  })
 
-  }
-
-  return Joi.validate(data, schema)
+  return schema.validate(data)
 }
 
 const loginValidation = (data) => {
