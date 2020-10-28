ac5c4fe5352dff9620c53eae043defa7366502d9
Ali Alrawaf <alrawafali@gmail.com>
2020-10-28 02:48:23 -0400

    user activation routes added including confirm user and resend. token model, nodemailer for activation emails, and host auths in process.env

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken')
-const User = require('../models/user')
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const nodemailer = require("nodemailer");
+const User = require('../models/user');
+const activationToken = require('../models/token');
 const {registrationValidation, loginValidation} = require('../validations/user_validations');
 
 router.post('/register', async (req, res) => {
@@ -29,13 +32,103 @@ router.post('/register', async (req, res) => {
     password: hashed_password,
   });
 
+  const token = new activationToken({
+    _userId: user._id,
+    token: crypto.randomBytes(16).toString('hex')
+  });
+
   try{
-    const saved_user = await user.save();
-    res.send({user: user.id});
+    await user.save();
+    await token.save();
   }catch(err){
     res.status(400).send(err);
   }
 
+  var transporter = nodemailer.createTransport({ 
+    service: 'Gmail', 
+    auth: { 
+      user: process.env.SPORTCRED_EMAIL, 
+      pass: process.env.SPORTCRED_PASS 
+    } 
+  });
+
+  var mailOptions = { 
+    from: 'no-reply@sportcred.com', 
+    to: user.email, 
+    subject: 'Account Verification Link', 
+    text: 'Hello '+ req.body.username +',\n\n' + 'Please verify your account by clicking the link: ' + 
+          '\nhttp:\/\/localhost:5000\/user\/confirm\/' + user.email + '\/' + token.token + '\n\nThank You!\n' 
+  };
+
+  transporter.sendMail(mailOptions, function (err) {
+    if (err) return res.status(500).send('Technical Issue!, Please click on resend to verify your Email.');
+
+    return res.status(200).send({
+      token: token.token,
+      text: 'A verification email has been sent to ' + user.email +
+            '. It will expire after one day. If you do not get the verification email, click on resend.'
+    });
+  });
+});
+
+router.post('/resend-activation', async (req, res) => {
+  const user = await User.findOne({email: req.body.email});
+  if (!user) return res.status(400).send('username does not exist');
+
+  if (user.isVerified) return res.status(200).send('This account has been already verified. Please log in.');
+
+  var token = new activationToken({
+    _userId: user._id, 
+    token: crypto.randomBytes(16).toString('hex') 
+  });
+
+  try{
+    await token.save();
+  }catch(err){
+    res.status(400).send(err);
+  }
+
+  var transporter = nodemailer.createTransport({ 
+    service: 'Gmail', 
+    auth: { 
+      user: process.env.SPORTCRED_EMAIL, 
+      pass: process.env.SPORTCRED_PASS
+    } 
+  });
+
+  var mailOptions = { 
+    from: 'no-reply@sportcred.com', 
+    to: user.email, 
+    subject: 'Account Verification Link', 
+    text: 'Hello '+ req.body.username +',\n\n' + 'Please verify your account by clicking the link: ' + 
+          '\nhttp:\/\/localhost:5000\/user\/confirm\/' + user.email + '\/' + token.token + '\n\nThank You!\n' 
+  };
+
+  transporter.sendMail(mailOptions, function (err) {
+    if (err) return res.status(500).send('Technical Issue!, Please click on resend to verify your Email.');
+
+    return res.status(200).send({
+      user: user._id,
+      text: 'A verification email has been sent to ' + user.email +
+            '. It will expire after one day. If you do not get the verification email, click on resend.'
+    });
+  });
+});
+
+router.get('/confirm/:email/:token', async (req, res) => {
+  const token = await Token.findOne({ token: req.params.token })
+  if (!token) return res.status(400).send('Your verification link may have expired. Please click on resend for verify your Email.');
+  
+  const user = await User.findOne({_id: token._userId, email: req.params.email})
+  if (!user) return res.status(400).send('We were unable to find a user for this verification. Please SignUp!');
+
+  if(user.activated) return res.status(200).send({text: 'Account is activated. Please Login'});
+
+  user.activated = true;
+  await user.save(function (err) {
+    if (err) return res.status(500).send(err.message);
+    else return res.status(200).send({user: user._id, text: 'Your account has been successfully verified'});
+  });
 });
 
 router.post('/login', async (req, res) => {
@@ -45,16 +138,17 @@ router.post('/login', async (req, res) => {
 
   // Find the user in the database
   const user = await User.findOne({username: req.body.username});
-  if (!user) return res.status(400).send('username or password is incorrect');
+  if (!user) return res.status(400).send('username does not exist');
 
   // Check password
   const valid_password = await bcrypt.compare(req.body.password, user.password);
   if (!valid_password) return res.status(400).send('username or password is incorrect');
 
+  if(!user.activated) return res.status(400).send('Your Email has not been verified. Please verify.');
+
   // Create a remember me token
   const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET)
   res.header('auth-token', token).send(token)
-
 });
 
 
