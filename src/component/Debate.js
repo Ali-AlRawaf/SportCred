e6beab4a0c693592aca9ac2e6f20fda97ccaa9ba
Mohamed Moussa <mohamed.moussa@mail.utoronto.ca>
2020-11-15 14:14:38 -0500

    [BIN-90] created frontend view of list of debates

---
+++ src/component/Debate.js
@@ -0,0 +1,107 @@
+import React, { useState, useCallback } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const Debate = (props) => {
+    const navigation = useNavigation();
+    const [textShown, setTextShown] = useState(false);
+    const [lengthMore, setLengthMore] = useState(false);
+    const toggleNumberOfLines = () => {
+        setTextShown(!textShown);
+        console.log(props)
+    }
+
+    const onTextLayout = useCallback(e => {
+        setLengthMore(e.nativeEvent.lines.length >= 4);
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.card}
+            onPress={() => navigation.navigate('Debate', {
+                _id: props._id
+            })}>
+                <View style={styles.textBox}>
+                    <Text style={styles.bodyText}
+                        onTextLayout={onTextLayout}
+                        numberOfLines={textShown ? undefined : 4}>
+                        {props.description}
+                    </Text>
+                    {
+                        lengthMore ? <Text
+                            onPress={toggleNumberOfLines}
+                            style={styles.toggle}
+                        >{textShown ? 'Read Less' : 'Read More'}</Text>
+                            : null
+                    }
+                </View>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 7
+    },
+    toggle: {
+        color: "#3D929A",
+        marginLeft: 10,
+    },
+    card: {
+        backgroundColor: '#242526',
+        width: 340,
+        minHeight: 120,
+        shadowColor: '#673939',
+        shadowOpacity: .1,
+        borderRadius: 1,
+        shadowRadius: 13,
+        flexDirection: 'row'
+    },
+    imgBox: {
+        width: 40,
+        height: 40,
+        left: 10,
+        top: 10,
+        borderRadius: 25
+    },
+    textBox: {
+        alignContent: 'space-between',
+        width: 320,
+        padding: 15
+    },
+    headerText: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        color: '#ffffff',
+        paddingLeft: 10,
+    },
+    bodyText: {
+        fontSize: 17,
+        paddingLeft: 10,
+        paddingTop: 10,
+        color: '#ffffff'
+    },
+    button: {
+        width: 130,
+        height: 30,
+        backgroundColor: '#3D929A',
+        borderRadius: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        left: 10,
+        position: 'relative',
+        bottom: 0,
+        marginTop: 5
+
+    },
+
+    buttonText: {
+        fontSize: 14,
+        color: '#ffffff',
+        fontWeight: 'bold'
+    },
+});
+
+
+export default Debate;
--- src/screens/DebatesScreen.js
+++ src/screens/DebatesScreen.js
@@ -1,36 +1,107 @@
 import * as React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import Debate from '../component/Debate';
+import profile_img from '../assets/profile_img.jpg';
+import logo_png from '../assets/logo_png.png';
+import StephenASmith from '../assets/StephenASmith.png';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Debate from './DebateScreen'
+import { ScrollView } from 'react-native-gesture-handler';
+import search_img from '../assets/search_18dp.png'
 import { useNavigation } from '@react-navigation/native';
 
-function DebatesScreen(){
+const debates = [
+    {
+        _id: "1",
+        description: "Who's A Better Guard: Kyrie Irving, Damian Lillard, or Stephen Curry?"
+    },
+    {
+        _id: "2",
+        description: 'Who is more important to the Lakers: LeBron James or Anthony Davis?'
+    },
+    {
+        _id: "3",
+        description: "Is Tyler Herro an upcoming all star? Is Tyler Herro an upcoming all star?" +
+            "Is Tyler Herro an upcoming all star? Is Tyler Herro an upcoming all star?" +
+            "Is Tyler Herro an upcoming all star? Is Tyler Herro an upcoming all star?" +
+            "Is Tyler Herro an upcoming all star? Is Tyler Herro an upcoming all star?" +
+            "Is Tyler Herro an upcoming all star? Is Tyler Herro an upcoming all star?" +
+            "Is Tyler Herro an upcoming all star? Is Tyler Herro an upcoming all star?",
+    },
+    {
+        _id: "4",
+        description: "Who will be on the front cover of NBA 2K22?"
+    },
+    {
+        _id: "5",
+        description: "Who is more important to the Lakers: LeBron James or Anthony Davis?"
+    },
+    {
+        _id: "6",
+        description: "Who's A Better Guard: Kyrie Irving, Damian Lillard, or Stephen Curry?"
+    },
+    {
+        _id: "7",
+        description: "Is Tyler Herro an upcoming all star? Is Tyler Herro an upcoming all star?" +
+        "Is Tyler Herro an upcoming all star? Is Tyler Herro an upcoming all star?" +
+        "Is Tyler Herro an upcoming all star? Is Tyler Herro an upcoming all star?" +
+        "Is Tyler Herro an upcoming all star? Is Tyler Herro an upcoming all star?" +
+        "Is Tyler Herro an upcoming all star? Is Tyler Herro an upcoming all star?" +
+        "Is Tyler Herro an upcoming all star? Is Tyler Herro an upcoming all star?"
+    },
+    {
+        _id: "8",
+        description: "Who will be on the front cover of NBA 2K22?"
+    },
+    {
+        _id: "9",
+        description: "Who is more important to the Lakers: LeBron James or Anthony Davis?"
+    }
+]
 
-	const navigation = useNavigation();
-	
+function DebatesScreen() {
+    const navigation = useNavigation();
 
-	handleSubmit = () => {
-		
-		navigation.navigate("Debate", {id: 1 })
-	}
-	
-	return (
-    <View style={styles.debates}>
-        <TouchableOpacity onPress={() => handleSubmit()}>
-        	<Text>Debate</Text>
-        </TouchableOpacity>
-    </View>
+    const listItems = debates.map((d, idx) => <Debate key={idx} description={d.description}></Debate>);
+
+    return (
+        <View style={styles.screen}>
+            <View
+                style={styles.header}
+            >
+            </View>
+            <ScrollView>
+                <View style={styles.debates}>
+                    {listItems}
+                </View>
+
+            </ScrollView>
+        </View >
     );
 }
 
 const styles = StyleSheet.create({
-	debates: {
+    debates: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         marginTop: 40
     },
+    screen: {
+        backgroundColor: '#333436',
+        height: 900
+    },
+    search_img: {
+        height: 20,
+        width: 20,
+        alignSelf: 'flex-end',
+        tintColor: 'white'
+    },
+    header: {
+        justifyContent: "flex-end",
+        marginRight: 10,
+        marginTop: 10
+    }
 })
 
 export default DebatesScreen;
